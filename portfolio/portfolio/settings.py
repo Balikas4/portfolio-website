0cb43d4bdf51eaca282dc4428d90a00ad5003e17
Balikas <balikas4@gmail.com>
2024-07-24 10:30:12 +0300

    add: certbot config

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -34,9 +34,9 @@ STATICFILES_DIRS = [
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webdevbalys.online', 'www.webdevbalys.online', 'localhost']
 
 
 # Application definition
