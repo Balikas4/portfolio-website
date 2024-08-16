31a0259b9df7420dadd39e08aed13b8d54749466
Balikas <balikas4@gmail.com>
2024-08-16 17:28:59 +0300

    edit: form

--- portfolio/website/forms.py
+++ portfolio/website/forms.py
@@ -5,3 +5,8 @@ class EmailForm(forms.ModelForm):
     class Meta:
         model = Email
         fields = ['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Your Name', 'class': 'form-input'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Your Email', 'class': 'form-input'}),
+            'message': forms.Textarea(attrs={'placeholder': 'Your Message', 'class': 'form-input', 'rows': 4}),
+        }
